5ad1cf28c909cefc08fb1560307206e1f9ce0100
David Gofman <dgofman@apple.com>
2015-01-12 22:02:58 -0800

    added jsdoc templates

---
+++ tools/docs/Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function (grunt) {
+    var SRC = './src';
+    var DIST = './dist';
+    var TMPL = './templates';
+    
+    grunt.initConfig({
+
+        clean: {
+            jsdoc: {
+                src: DIST
+            }
+        },
+
+        less: {
+            dist: {
+                src: TMPL + '/less/**/custom.less',
+                dest: TMPL + '/static/styles/custom.css'
+            }
+        },
+
+        jsdoc: {
+            dist: {
+                src: [SRC + '/**/*.js'],
+                options: {
+                    destination: DIST,
+                    configure: TMPL + '/conf.json',
+                    template: TMPL,
+                    'private': false
+                }
+            }
+        }
+    });
+
+    // Load task libraries
+    [
+        'grunt-contrib-clean',
+        'grunt-contrib-less',
+        'grunt-jsdoc'
+    ].forEach(function (taskName) {
+        grunt.loadNpmTasks(taskName);
+    });
+
+    grunt.registerTask('build', 'Create documentations', [
+        'clean:jsdoc',
+        'less',
+        'jsdoc:dist'
+    ]);
+};
